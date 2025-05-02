9fee7ca6829d7ddc6203a779fdd4a7746693e8de
josuemoraes <josuemoraes2@hotmail.com>
2025-05-02 09:52:58 -0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,10 +1,17 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+import os
+
 from .routes import camera, dashboard
 
 app = FastAPI()
 
+# Rotas da API
 app.include_router(camera.router, prefix="/camera")
 app.include_router(dashboard.router, prefix="/dashboard")
+
+# Servir arquivos estáticos
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/")
 def read_root():
