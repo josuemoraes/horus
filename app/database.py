f852e7db18ac74c4801c0bc45982642a56004ea2
josuemoraes <josuemoraes2@hotmail.com>
2025-05-02 09:44:33 -0300

    Update database.py

--- app/database.py
+++ app/database.py
@@ -2,7 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Cria as tabelas automaticamente
+from app.models import Base
+Base.metadata.create_all(bind=engine)
