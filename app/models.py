4f53062e0837759acb41d0f603bae35dfaf36008
josuemoraes <josuemoraes2@hotmail.com>
2025-05-02 10:54:24 -0300

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,1 +1,12 @@
-# models.py - placeholder
+from sqlalchemy import Column, Integer, String, DateTime
+from datetime import datetime
+from .database import Base
+
+class Detection(Base):
+    __tablename__ = "detections"
+
+    id = Column(Integer, primary_key=True, index=True)
+    camera_id = Column(String, index=True)
+    face_id = Column(String, index=True)
+    duration = Column(Integer)
+    timestamp = Column(DateTime, default=datetime.utcnow)
